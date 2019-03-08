49d545c57e65e66ebff4878f23a69633bd62be32
MrbigBent <e.b.egribey@gmail.com>
2019-03-08 21:34:20 +0300

    first commit

---
+++ src/main/java/com/emre/ppm/PpmProjectApplication.java
@@ -0,0 +1,12 @@
+package com.emre.ppm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmProjectApplication.class, args);
+	}
+}
